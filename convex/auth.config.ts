994adf796890bf52e010f71a4a6a1d0b054a4254
VarishTyagi <varishtyagi351@gmail.com>
2024-07-15 15:05:05 +0530

    first commit

---
+++ convex/auth.config.ts
@@ -0,0 +1,12 @@
+
+
+const authConfig = {
+    providers: [
+        {
+            domain: "https://grand-gnat-49.clerk.accounts.dev",
+            applicationID: "convex",
+        },
+    ]
+};
+
+export default authConfig;
--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,27 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    typescript: {
+        ignoreBuildErrors: true,
+    },
+    
+    images: {
+        remotePatterns: [
+            {
+                protocol: 'https',
+                hostname: 'lovely-flamingo-139.convex.cloud'
+            },
+
+            {
+                protocol: 'https',
+                hostname: 'adjoining-mole-258.convex.cloud'
+            },
+
+            {
+                protocol: 'https',
+                hostname: 'img.clerk.com'
+            }
+        ]
+    }
+};
 
 export default nextConfig;
